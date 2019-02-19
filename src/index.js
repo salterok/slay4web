7290d6234f1f02ca5cb20afff080df161847504c
Sergiy Samborskiy <salterok@gmail.com>
2019-02-19 22:24:02 +0200

    Add background to whole scene
    Temp fix for pixi-viewport version incompatibility.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
+/*
+ * @Author: Sergiy Samborskiy 
+ * @Date: 2019-02-19 21:38:49 
+ * @Last Modified by: Sergiy Samborskiy
+ * @Last Modified time: 2019-02-19 22:20:56
+ */
+
+import "./patcher";
 import * as PIXI from "pixi.js";
 import * as Viewport from "imports-loader?PIXI=pixi.js!pixi-viewport";
 import * as Honeycomb from "honeycomb-grid";
 import { Hex } from "./Hex";
 
+const width = window.innerWidth;
+const height = window.innerHeight;
+
 const app = new PIXI.Application({
-    width: window.innerWidth,
-    height: window.innerHeight,
+    width: width,
+    height: height,
 });
 document.body.appendChild(app.view);
 
+
+const background = new PIXI.Graphics();
+
+background.beginFill(0x33A1DE);
+background.drawRect(0, 0, width, height);
+
+app.stage.addChild(background);
+
 var viewport = new Viewport({
-    screenWidth: window.innerWidth,
-    screenHeight: window.innerWidth,
+    screenWidth: width,
+    screenHeight: height,
     worldWidth: 20 * 30 + 1000,
     worldHeight: 20 * 30 + 1000,
 
