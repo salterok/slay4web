0968207304fc785cb5aab83a6d1921756a321c54
Sergiy Samborskiy <salterok@gmail.com>
2019-03-22 14:09:04 +0200

    Prepare some code for player actions hanling

---
+++ src/types.d.ts
@@ -0,0 +1,25 @@
+/*
+ * @Author: Sergiy Samborskiy 
+ * @Date: 2019-02-26 03:48:24 
+ * @Last Modified by: Sergiy Samborskiy
+ * @Last Modified time: 2019-02-26 06:08:41
+ */
+
+interface TurnAction {
+    type: string;
+}
+
+interface TurnActionResponse {
+    timeLeft: number;
+}
+
+interface TurnContract<T, R = TurnActionResponse> extends AsyncIterator<T> {
+    next(value: R): Promise<IteratorResult<T>>;
+
+}
+
+type TurnContractFactory = (initial: TurnActionResponse) => TurnContract<TurnAction>;
+
+interface PlayerController {
+    getActions(initial: TurnActionResponse): TurnContract<TurnAction>;
+}
