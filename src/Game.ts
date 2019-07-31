3e4adaa7d46cea472aef974147636f4f38e30e64
Sergiy Samborskiy <salterok@gmail.com>
2019-07-31 04:53:15 +0300

    Make first version of zones designations among players on map

--- src/Game.ts
+++ src/Game.ts
@@ -2,14 +2,15 @@
  * @Author: Sergiy Samborskiy 
  * @Date: 2019-02-26 03:32:36 
  * @Last Modified by: Sergiy Samborskiy
- * @Last Modified time: 2019-07-22 05:56:21
+ * @Last Modified time: 2019-07-25 11:07:29
  */
 
 import * as PIXI from "pixi.js";
 import "honeycomb-grid";
-import { Point, Hex,  } from "honeycomb-grid";
 import * as g from "honeycomb-grid";
-import { GameMap, Tile } from "./mapGenerator";
+import { GameMap, Tile, GameHex } from "./mapGenerator";
+import { sample } from "./utils";
+import { groupHexes } from "./Map/utils";
 
 enum Actions {
     MoveUnit = "moveUnit",
@@ -53,25 +54,73 @@ function delay(delay: number) {
 }
 
 export class Game {
-    zones = new WeakMap<Tile, Zone>();
+    zones = new WeakMap<GameHex, Zone>();
     players: { id: number; controller: PlayerController }[];
     
     constructor(public map: GameMap, players: PlayerController[]) {
-        this.players = players.map((player, index) => {
+        let colors = [0xec5c5c, 0xecba5c, 0xdbec5c, 0x7eec5c, 0x5cdeec, 0x5c94ec, 0x875cec, 0xd75cec, 0xec5c89];
+        let preparedPlayers = this.players = players.map((player, index) => {
             return {
                 id: index,
+                color: colors.splice(0, 1),
                 controller: player,
             };
         });
+
+        Tile.prototype.getOwnerInfo = function () {
+            return preparedPlayers.find(p => p.id === this.owner) || {};
+        }
 
         this.placeZones();
     }
 
     private placeZones() {
-        
+        const startingZoneCount = 3;
+        const numPlayers = this.players.length;
+
+        const zonesToReserve = numPlayers * startingZoneCount;
+
+        const posiblePlaces = new Map<number, GameHex[]>(this.players.map((p, i) => ([i, [] ])))
+        const choosedZones = new Map<number, Zone[]>(this.players.map((p, i) => ([i, [] ])))
 
         for (const hex of this.map) {
-            hex.model.owner = this.players[0].id;
+            hex.model.owner = sample(this.players).id;
+
+            posiblePlaces.get(hex.model.owner).push(hex);
+        }
+
+
+        for (const player of this.players) {
+            for (let i = 0; i < startingZoneCount; i++) {
+                const places = posiblePlaces.get(player.id);
+                const groups = groupHexes(this.map, places);
+
+                groups.sort((g1, g2) => g2.length - g1.length);
+
+                let choosedGroup = groups.find(g => g.length >= 2);
+
+                // TODO: find the most distant starting zones for same player
+
+                if (!choosedGroup) {
+                    console.warn(`Can't find starting zone for player ${player.id} at itter ${i}`);
+                }
+
+                if (choosedGroup) {
+                    posiblePlaces.set(player.id, places.filter(pl => !choosedGroup.includes(pl)));
+
+                    // TODO: select better place for capital
+                    // probably one thaat not far away from weighted center of zone but close to edge
+
+                    const zone = new Zone(choosedGroup[0]);
+
+                    choosedGroup.slice(1).forEach(hex => {
+                        this.zones.set(hex, zone);
+                        zone.addTile(hex)
+                    });
+
+                    choosedZones.get(player.id).push(zone);
+                }
+            }
         }
 
         
@@ -90,7 +139,8 @@ export class Game {
 }
 
 class Zone {
-    constructor(public capital: Hex) {
+    constructor(public capital: GameHex) {
+        capital.model.isCapital = true;
     }
 
     get isControllable() {
